3a268ae8c5ee13575b99bde96466214a966d9be3
aprnbji <aparnap.balaji@gmail.com>
2025-08-06 19:52:03 +0530

    feat: updated main

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ def get_chat_messages(project_id: int, db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
